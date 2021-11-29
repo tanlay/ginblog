86bef6484bbaa5f0d7d0606bd812677350b7a37b
tanlay <18313178130@163.com>
2021-11-29 17:56:52 +0800

    处理日志系统（自定义日志、按时间分割日志、软连接最新日志）

--- go.mod
+++ go.mod
@@ -22,9 +22,14 @@ require (
 	github.com/jinzhu/now v1.1.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/lestrrat-go/strftime v1.0.5 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
 	golang.org/x/sys v0.0.0-20211020174200-9d6173849985 // indirect
---
+++ middleware/logger.go
@@ -0,0 +1,89 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	rotalog "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+	"math"
+	"os"
+	"time"
+)
+
+//Logger 自定义日志格式，输出到文件
+func Logger() gin.HandlerFunc {
+	filePath := "log/ginblog"
+	linkName := "latest_log.log"
+	src, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE,0755)
+	if err != nil {
+		fmt.Println("err：", err.Error())
+	}
+	logger := logrus.New()
+
+	//不记录在控制台
+	logger.Out = src
+
+	logger.SetLevel(logrus.DebugLevel)
+	logWriter, _ := rotalog.New(
+		filePath+"%Y%m%d.log",
+		rotalog.WithMaxAge(7*24*time.Hour),
+		rotalog.WithRotationTime(24*time.Hour),
+		rotalog.WithLinkName(linkName),
+		)
+
+		writeMap := lfshook.WriterMap{
+			logrus.InfoLevel: logWriter,
+			logrus.FatalLevel: logWriter,
+			logrus.DebugLevel: logWriter,
+			logrus.WarnLevel: logWriter,
+			logrus.ErrorLevel: logWriter,
+			logrus.PanicLevel: logWriter,
+		}
+
+		hook := lfshook.NewHook(writeMap, &logrus.TextFormatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+		})
+		logger.AddHook(hook)
+	return func(c *gin.Context) {
+		startTime := time.Now()
+		c.Next()
+		stopTime := time.Since(startTime)
+		costTime := fmt.Sprintf("%d", int(math.Ceil(float64(stopTime.Nanoseconds())/1000000.0)))
+		hostName, err := os.Hostname()
+		if err != nil {
+			hostName = "unKnown"
+		}
+		statusCode := c.Writer.Status()
+		clientIP := c.ClientIP()
+		userAgent := c.Request.UserAgent()
+		dataSize := c.Writer.Size()
+		if dataSize < 0 {
+			dataSize = 0
+		}
+		method := c.Request.Method
+		path := c.Request.RequestURI
+
+		entry := logger.WithFields(logrus.Fields{
+			"HostName": hostName,
+			"status": statusCode,
+			"CostTime": costTime,
+			"IP": clientIP,
+			"Method": method,
+			"Path": path,
+			"DataSize": dataSize,
+			"UserAgent": userAgent,
+		})
+		if len(c.Errors) > 0 {
+			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
+		}
+		if statusCode >= 500{
+			entry.Error()
+		} else if  statusCode >= 400 {
+			entry.Warn()
+		} else {
+			entry.Info()
+		}
+	}
+
+}
